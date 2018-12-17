76907fa57498ad0a5bae3aaac483c1484ff1985f
mlibai <mlibai@163.com>
2018-12-17 17:34:25 +0800

    优化

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	entry: __dirname + "/Native/index.js",  // 已多次提及的唯一入口文件
+	entry: __dirname + "/native/index.js",  // 已多次提及的唯一入口文件
 	output: {
 		path: __dirname + "/Products", 		// 打包后的文件存放的地方
 		filename: "native.js" 				// 打包后输出文件的文件名
